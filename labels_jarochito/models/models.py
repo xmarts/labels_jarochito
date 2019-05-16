4b25251874f12e8f0f8d9f74fe74e7d73c50bfc7
Marco <marcocaaguilar@gmail.com>
2019-05-15 23:52:09 -0500

    Creación de numeros consecutivos para name

--- labels_jarochito/models/models.py
+++ labels_jarochito/models/models.py
@@ -6,28 +6,27 @@ class LabelsPallets(models.Model):
 
 	_name = 'labels.pallets'
 
-	name = fields.Char( string = 'Nombre' )
+	def _folio_default(self):
+		cr = self.env.cr
+		cr.execute('select "id" from "labels_pallets" order by "id" desc limit 1')
+		id_returned = cr.fetchone()
+		if id_returned == None:
+			id_returned = (0,)
+		text = ''
+		pref = 'Etiq-'
+		if((max(id_returned) + 1) < 100):
+			text = pref + '00' + str(max(id_returned) + 1)
+		else:
+			text = pref + str(max(id_returned) + 1)
+		return text
+
+	name = fields.Char( string = 'Nombre' , readonly = True, default = _folio_default )
 
 	turn = fields.Selection( [('mat','Matutino'),('ves','Vespertino'),('noc','Nocturno')] , string = 'Turno' )
 
 	pallet = fields.Many2one( 'pallets' , string = 'Tarima' )
 
 	date = fields.Date( string = 'Fecha' )
-
-	def _get_consecutivo_num(self, cr, uid, context=None):
-		last_id = 0
-		get_count = self.search(cr, uid, [(1, '=', 1)], order='id')
-		if get_count:
-			for item in self.browse(cr, uid, get_count, context):
-				sec = item.consecutivo.split('-')
-				sec_num = int(sec[1]) + 1
-				last_id = sec_num
-		else:
-			last_id = 1
-		prefijo = 'EST-'
-		serie = last_id
-		consecutivo = prefijo + str(serie).rjust(5, '0')
-		return consecutivo
 
 class Pallets(models.Model):
 
