47a0dde5cf18c78029b3190403e7b03704176039
Marco <marcocaaguilar@gmail.com>
2019-05-15 22:50:07 -0500

    Creación del manifest

--- labels_jarochito/__manifest__.py
+++ labels_jarochito/__manifest__.py
@@ -24,9 +24,11 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'reports/layout.xml',
+        'reports/print_pallets.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
