9103654ba5ebafc8bb84af94511856c74e39fc9e
Marco <marcocaaguilar@gmail.com>
2019-05-15 22:50:27 -0500

    Creación modelo de etiquetas

--- labels_jarochito/models/models.py
+++ labels_jarochito/models/models.py
@@ -1,15 +1,38 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
-# class labels_jarochito(models.Model):
-#     _name = 'labels_jarochito.labels_jarochito'
+class LabelsPallets(models.Model):
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+	_name = 'labels.pallets'
+
+	name = fields.Char( string = 'Nombre' )
+
+	turn = fields.Selection( [('mat','Matutino'),('ves','Vespertino'),('noc','Nocturno')] , string = 'Turno' )
+
+	pallet = fields.Many2one( 'pallets' , string = 'Tarima' )
+
+	date = fields.Date( string = 'Fecha' )
+
+	def _get_consecutivo_num(self, cr, uid, context=None):
+		last_id = 0
+		get_count = self.search(cr, uid, [(1, '=', 1)], order='id')
+		if get_count:
+			for item in self.browse(cr, uid, get_count, context):
+				sec = item.consecutivo.split('-')
+				sec_num = int(sec[1]) + 1
+				last_id = sec_num
+		else:
+			last_id = 1
+		prefijo = 'EST-'
+		serie = last_id
+		consecutivo = prefijo + str(serie).rjust(5, '0')
+		return consecutivo
+
+class Pallets(models.Model):
+
+	_name = 'pallets'
+
+	pallet_name = fields.Char( string = 'Tarima' )
+
+	_rec_name = 'pallet_name'
