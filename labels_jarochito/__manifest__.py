c99fd61c60d9e91a3b8cf66fc869edde3c38a173
Marco <marcocaaguilar@gmail.com>
2019-05-15 23:01:30 -0500

    Cambio manifest

--- labels_jarochito/__manifest__.py
+++ labels_jarochito/__manifest__.py
@@ -3,20 +3,19 @@
     'name': "labels_jarochito",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Crea etiquetas para tarimas""",
 
     'description': """
-        Long description of module's purpose
+        Creación de etiquetas para tarimas, con datos como turno, fecha y tarima a seleccionar.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Xmarts, Colaborador : Marco Aguilar",
+    'website': "http://www.xmarts.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Stock',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -34,4 +33,6 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'installable' : True,
+    'application' : True
 }
